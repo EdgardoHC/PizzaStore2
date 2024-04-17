606c70095c17d83e501cd3728bc067c8bc78c046
jhenriquez <jhenriquez@STINFOR017EE>
2024-04-17 15:12:24 -0600

    Agregar archivos de proyecto.

---
+++ PizzaDb.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaStore2;
+
+class PizzaDb : DbContext
+{
+    public PizzaDb(DbContextOptions options) : base(options) { }
+    public DbSet<Pizza> Pizzas { get; set; } = null!;
+}
